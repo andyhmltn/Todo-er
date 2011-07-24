6a22d4cc4fea9b0d389d281617ada05b5c0a79e1
Andy Hamilton <andyhmltn@gmail.com>
2011-07-24 01:42:41 +0100

    IMPORTANT: I have removed 'remove_column' for user_id on lists in this commit. If you are going from No collaborations - Collaborations user the last commits migration.

--- db/migrate/20110723231135_create_collaborators.rb
+++ db/migrate/20110723231135_create_collaborators.rb
@@ -1,6 +1,5 @@
 class CreateCollaborators < ActiveRecord::Migration
   def self.up
-    remove_column("lists", "user_id")
     create_table :collaborators do |t|
       t.integer :user_id
       t.integer :list_id
